dacf8ba986487259e95f29cdc9068d0ecaaaf5c5
SeungGun <saverrw2864@naver.com>
2022-07-10 15:54:02 +0900

    feat: create method of request for sign-up in retrofit

--- Breaking/app/src/main/java/com/dope/breaking/retrofit/RetrofitService.kt
+++ Breaking/app/src/main/java/com/dope/breaking/retrofit/RetrofitService.kt
@@ -1,7 +1,10 @@
 package com.dope.breaking.retrofit
 
 import com.dope.breaking.model.*
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 interface RetrofitService {
@@ -22,7 +25,33 @@ interface RetrofitService {
      * @author - Seunggun Sin
      */
     @POST("oauth/sign-in/google")
-    fun requestGoogleLogin(@Body tokens: RequestGoogleToken): Call<ResponseLogin>
+    suspend fun requestGoogleLogin(@Body tokens: RequestGoogleToken): Response<ResponseLogin>
+
+    /**
+     * 최종 회원가입 요청 메소드 - Multipart 요청
+     * @param image(MultipartBody.Part): 이미지가 담긴 multi form 요청 body (key=profileImg)
+     * @param data(RequestBody): 나머지 텍스트 필드 값이 담긴 요청 body (key=signUpRequest)
+     * @response - String: 응답 body 자체는 중요 x, Jwt 토큰을 위한 헤더 값만 필요
+     * @author - Seunggun Sin
+     */
+    @Multipart
+    @POST("oauth2/sign-up")
+    fun requestSignUp(
+        @Part image: MultipartBody.Part,
+        @Part("signUpRequest") data: RequestBody
+    ): Response<String>
+
+    /**
+     * 앱을 재시작하고 자동 로그인 시, Jwt 토큰 만료 확인을 위한 요청 메소드
+     * @param token(String): "authorization" 헤더 키로 Jwt 토큰 값을 넣음
+     * @response - ResponseValidateJwt
+     * @author - Seunggun Sin
+     */
+    @POST(
+        "oauth2" +
+                "/validate-jwt"
+    )
+    fun requestValidationJwt(@Header("authorization") token: String): Response<ResponseValidateJwt>
 
     /**
      * OAuth2 구글 API 서버로부터 엑세스 토큰 요청 메소드
@@ -31,5 +60,5 @@ interface RetrofitService {
      * @author - Seunggun Sin
      */
     @POST("token")
-    fun requestGoogleAccessToken(@Body googleRequest: RequestGoogleAccessToken): Call<ResponseGoogleAccessToken>
+    suspend fun requestGoogleAccessToken(@Body googleRequest: RequestGoogleAccessToken): Response<ResponseGoogleAccessToken>
 }
