ba9b489fad0564dc7caf57a1d57967018f5f3c26
SeungGun <saverrw2864@naver.com>
2022-07-10 15:49:47 +0900

    feat: add implementation of Serializable to ResponseLogin class

--- Breaking/app/src/main/java/com/dope/breaking/model/ResponseLogin.kt
+++ Breaking/app/src/main/java/com/dope/breaking/model/ResponseLogin.kt
@@ -1,10 +1,11 @@
 package com.dope.breaking.model
 
 import com.google.gson.annotations.SerializedName
+import java.io.Serializable
 
-data class ResponseLogin (
-    @SerializedName("fullname") var fullName:String, // 유저 성 이름
-    @SerializedName("username") var userName:String, // 회원번호
-    @SerializedName("email") var userEmail:String,   // 유저 이메일
-    @SerializedName("profileImgURL") var profileImgUrl:String // 프로필 이미지 Url
-)
+data class ResponseLogin(
+    @SerializedName("fullname") var fullName: String, // 유저 성 이름
+    @SerializedName("username") var userName: String, // 회원번호
+    @SerializedName("email") var userEmail: String,   // 유저 이메일
+    @SerializedName("profileImgURL") var profileImgUrl: String // 프로필 이미지 Url
+) : Serializable
