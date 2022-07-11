cda4945c3b20a9e966ba4c83e4e518b2788aafa3
SeungGun <saverrw2864@naver.com>
2022-07-11 19:35:58 +0900

    feat: request for jwt token validation for auto-login in JwtTokenUtil.kt

--- Breaking/app/src/main/java/com/dope/breaking/util/JwtTokenUtil.kt
+++ Breaking/app/src/main/java/com/dope/breaking/util/JwtTokenUtil.kt
@@ -1,7 +1,12 @@
 package com.dope.breaking.util
 
 import android.content.Context
+import com.dope.breaking.exception.ResponseErrorException
+import com.dope.breaking.model.ResponseJwtUserInfo
+import com.dope.breaking.retrofit.RetrofitManager
+import com.dope.breaking.retrofit.RetrofitService
 import okhttp3.Headers
+import kotlin.jvm.Throws
 
 /**
  * Jwt 토큰과 관련된 처리를 하는 Util 클래스
@@ -17,8 +22,8 @@ class JwtTokenUtil(context: Context) {
      * 로컬 파일, 즉, SharedPreferences 로부터 토큰 값을 가져오는 메소드
      * @param - None
      * @return String?: 로컬에 저장된 jwt 토큰 값 리턴, 없다면 null 리턴
-     * @author - Seunggun Sin
-     * @since - 2022-07-09
+     * @author Seunggun Sin
+     * @since 2022-07-09
      */
     fun getTokenFromLocal(): String? {
         val value = prefUtil.getString("token", "none")
@@ -26,11 +31,39 @@ class JwtTokenUtil(context: Context) {
     }
 
     /**
+     * 백엔드 서버에 Jwt 토큰 만료 확인 등, 재 로그인 및 자동 로그인에 사용되는 토큰 검증 메소드
+     * @param token(String): Jwt 토큰 문자열 값
+     * @return ResponseJwtUserInfo: 응답으로 받은 유저의 기본 정보 DTO 객체
+     * @throws ResponseErrorException: 정상 응답 (2xx) 이외의 응답이 왔을 때 exception 발생
+     * @author Seunggun Sin
+     * @since 2022-07-11
+     */
+    @Throws(ResponseErrorException::class)
+    suspend fun validateJwtToken(token: String): ResponseJwtUserInfo {
+        val service = RetrofitManager.retrofit.create(RetrofitService::class.java)
+        val response = service.requestValidationJwt(token)
+
+        if (response.code() in 200..299) {
+            if (response.body() != null) {
+                return response.body()!!
+            } else {
+                throw ResponseErrorException("응답 Body가 존재하지 않습니다!")
+            }
+        } else {
+            throw ResponseErrorException(
+                "요청에 실패하였습니다. code: ${response.code()}\nerror: ${
+                    response.errorBody()?.string()
+                }"
+            )
+        }
+    }
+
+    /**
      * Http 요청의 응답의 헤더로부터 Jwt 토큰 값을 가져오는 메소드
      * @param headers(Headers): 응답 헤더 객체
      * @return String?: 헤더에 담긴 jwt 토큰 값 리턴, 없다면 null 리턴
-     * @author - Seunggun Sin
-     * @since - 2022-07-09
+     * @author Seunggun Sin
+     * @since 2022-07-09
      */
     fun getTokenFromResponse(headers: Headers): String? {
         return headers[keyName]
@@ -40,8 +73,8 @@ class JwtTokenUtil(context: Context) {
      * 로컬에 토큰 값을 저장하는 메소드 (key=token)
      * @param token(String): 저장하고자 하는 jwt 토큰 문자열
      * @return - None
-     * @author - Seunggun Sin
-     * @since - 2022-07-09
+     * @author Seunggun Sin
+     * @since 2022-07-09
      */
     fun setToken(token: String) {
         prefUtil.setString("token", token)
@@ -52,8 +85,8 @@ class JwtTokenUtil(context: Context) {
      * @param headerName(String): jwt 토큰을 위한 헤더 key 값으로, "authorization"로 고정
      * @param headers(Headers): 백엔드 서버로 요청의 결과로 받은 응답의 헤더 객체인 response.headers().
      * @return 응답의 결과로 토큰이 있는지 없는지에 대한 bool 값 리턴
-     * @author - Seunggun Sin
-     * @since - 2022-07-07
+     * @author Seunggun Sin
+     * @since 2022-07-07
      */
     fun hasJwtToken(headerName: String, headers: Headers): Boolean =
     // Map 데이터로 인덱스 연산자에 문자열을 넣는 key-value 방식을 사용하여 헤더 map 데이터에 headerName 문자열을 넣었을 때
