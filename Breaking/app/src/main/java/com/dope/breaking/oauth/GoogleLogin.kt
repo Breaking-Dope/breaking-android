aebdfc7f4ec51c30d7991b9f126ce49ac3c7973e
SeungGun <saverrw2864@naver.com>
2022-07-11 19:42:05 +0900

    refactor: rollback google token login and add exception with throws, comment

--- Breaking/app/src/main/java/com/dope/breaking/oauth/GoogleLogin.kt
+++ Breaking/app/src/main/java/com/dope/breaking/oauth/GoogleLogin.kt
@@ -2,6 +2,7 @@ package com.dope.breaking.oauth
 
 import android.content.Context
 import com.dope.breaking.BuildConfig
+import com.dope.breaking.exception.ResponseErrorException
 import com.dope.breaking.model.RequestGoogleAccessToken
 import com.dope.breaking.model.RequestGoogleToken
 import com.dope.breaking.model.ResponseGoogleAccessToken
@@ -12,6 +13,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import okhttp3.Headers
 import retrofit2.Response
 
@@ -33,10 +37,12 @@ class GoogleLogin(private val context: Context) {
     /**
      * OAuth2 구글 로그인 프로세스 과정에 대한 함수 (엑세스 토큰 요청 → 토큰 검증 요청 → Jwt 토큰 유무 체크)
      * @param account(GoogleSignInAccount): 구글 로그인 시 얻는 계정 정보 객체
-     * @return - Boolean: 구글 로그인 과정을 통해 사용자가 회원가입이 필요한지 필요하지 않은지에 대한 bool 값 리턴(필요하면 false 리턴)
+     * @return Boolean: 구글 로그인 과정을 통해 사용자가 회원가입이 필요한지 필요하지 않은지에 대한 bool 값 리턴(필요하면 false 리턴)
+     * @throws ResponseErrorException: 정상 응답 (2xx) 이외의 응답이 왔을 때 exception 발생
      * @author Seunggun Sin
-     * @since 2022-07-07 | 2022-07-10
+     * @since 2022-07-07 | 2022-07-11
      */
+    @Throws(ResponseErrorException::class)
     suspend fun requestGoogleLogin(account: GoogleSignInAccount): Boolean {
         /*
         구글의 엑세스 토큰을 얻기 위해 구글 api 서버에 요청하는 메소드 호출
@@ -56,13 +62,19 @@ class GoogleLogin(private val context: Context) {
             accessTokenResponseObject.idToken,
             accessTokenResponseObject.accessToken
         )
-
         if (validationResponse.errorBody() == null) {
             responseBody = validationResponse.body()!!
         }
-
-        // 토큰이 있으면 true, 없으면 false 리턴
-        return hasJwtToken(jwtHeaderKey, validationResponse.headers())
+        if (validationResponse.code() in 200..299) {
+            // 토큰이 있으면 true, 없으면 false 리턴
+            return hasJwtToken(jwtHeaderKey, validationResponse.headers())
+        } else {
+            throw ResponseErrorException(
+                "요청에 실패하였습니다. code: ${validationResponse.code()}\nerror: ${
+                    validationResponse.errorBody()?.string()
+                }"
+            )
+        }
     }
 
     /**
@@ -72,9 +84,9 @@ class GoogleLogin(private val context: Context) {
      * @param clientId(String): 구글 로그인에 대한 우리 프로젝트의 클라이언트 ID
      * @param clientSecret(String): 구글 로그인에 대한 우리 프로젝트의 클라이언트 보안 비밀번호
      * @param code(String): 사용자에 의한 구글 로그인 요청으로 받은 서버 인가 코드 값 (serverAuthCode)
-     * @return - ResponseGoogleAccessToken? 객체로 id 토큰과 access 토큰 필드가 담겨 있다.
-     * @author - Seunggun Sin
-     * @since - 2022-07-07 | 2022-07-10
+     * @return ResponseGoogleAccessToken? 객체로 id 토큰과 access 토큰 필드가 담겨 있다.
+     * @author Seunggun Sin
+     * @since 2022-07-07 | 2022-07-10
      */
     private suspend fun getGoogleAccessToken(
         clientId: String,
@@ -102,28 +114,31 @@ class GoogleLogin(private val context: Context) {
      * 구글로부터 받아온 id 토큰과 access 토큰을 바탕으로 백엔드 서버에 로그인을 요청한다. (토큰 검증)
      * retrofit 요청 메소드의 리턴을 Response 로 설정하여 값을 바로 받아오도록 수정
      * suspend 함수
+     * ※ 응답의 형태가 여러가지인 경우 Response 방식이 아닌 Call 방식 사용하여 코드에 따른 처리하기
      * @param idToken(String): ID 토큰
      * @param accessToken(String): Access 토큰
-     * @return - Response<ResponseLogin> 으로 응답 raw 데이터 반환
+     * @return Response<ResponseLogin> 으로 응답 raw 데이터 반환
      * @author Seunggun Sin
-     * @since - 2022-07-07 | 2022-07-10
+     * @since 2022-07-07 | 2022-07-11
      */
     private suspend fun requestTokenValidation(
         idToken: String,
         accessToken: String
     ): Response<ResponseLogin> {
-        // 백엔드 서버에 대한 retrofit 서비스 객체 생성
-        val service = RetrofitManager.retrofit.create(RetrofitService::class.java)
-        /*
-        parameter 로 받아온 값을 바탕으로 객체를 요청 body 로 전달 후 백엔드 서버에 로그인 요청
-        동기적으로 요청 → .execute() 메소드
-        */
-        return service.requestGoogleLogin(
-            RequestGoogleToken(
-                idToken,
-                accessToken
-            )
-        ) // 요청에 대한 응답 객체 리턴
+        return CoroutineScope(Dispatchers.Main).async(Dispatchers.IO) {
+            // 백엔드 서버에 대한 retrofit 서비스 객체 생성
+            val service = RetrofitManager.retrofit.create(RetrofitService::class.java)
+            /*
+            parameter 로 받아온 값을 바탕으로 객체를 요청 body 로 전달 후 백엔드 서버에 로그인 요청
+            동기적으로 요청 → .execute() 메소드
+            */
+            return@async service.requestGoogleLogin(
+                RequestGoogleToken(
+                    idToken,
+                    accessToken
+                )
+            ).execute() // 응답 결과 자체를 리턴
+        }.await() // async 블럭의 코드가 실행될 때까지 기다리고 끝나면 결과 값 리턴
     }
 
     /**
@@ -131,11 +146,11 @@ class GoogleLogin(private val context: Context) {
      * @param headerName(String): jwt 토큰을 위한 헤더 key 값으로, "authorization"로 고정
      * @param headers(Headers): 백엔드 서버로 요청의 결과로 받은 응답의 헤더 객체인 response.headers().
      * @return 응답의 결과로 토큰이 있는지 없는지에 대한 bool 값 리턴
-     * @author - Seunggun Sin
-     * @since - 2022-07-07
+     * @author Seunggun Sin
+     * @since 2022-07-07
      */
     private fun hasJwtToken(headerName: String, headers: Headers): Boolean =
-    // Map 데이터로 인덱스 연산자에 문자열을 넣는 key-value 방식을 사용하여 헤더 map 데이터에 headerName 문자열을 넣었을 때
+        // Map 데이터로 인덱스 연산자에 문자열을 넣는 key-value 방식을 사용하여 헤더 map 데이터에 headerName 문자열을 넣었을 때
         // 나오는 value 의 값이 null 이거나 빈 문자열이라면 false 리턴, 그렇지 않고 정상적인 값이 있으면 true 리턴
         !(headers[headerName] == null || headers[headerName]!!.isEmpty())
 }
