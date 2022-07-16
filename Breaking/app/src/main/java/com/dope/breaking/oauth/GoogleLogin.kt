94e083c6cf97fa35c3637ef5ee9e8863a9726ff1
SeungGun <saverrw2864@naver.com>
2022-07-16 19:36:15 +0900

    refactor: add exception handling and parse response data as json by different response with error

--- Breaking/app/src/main/java/com/dope/breaking/oauth/GoogleLogin.kt
+++ Breaking/app/src/main/java/com/dope/breaking/oauth/GoogleLogin.kt
@@ -2,26 +2,30 @@ package com.dope.breaking.oauth
 
 import android.content.Context
 import com.dope.breaking.BuildConfig
+import com.dope.breaking.exception.InvalidAccessTokenException
 import com.dope.breaking.exception.ResponseErrorException
-import com.dope.breaking.model.RequestGoogleAccessToken
-import com.dope.breaking.model.RequestGoogleToken
-import com.dope.breaking.model.ResponseGoogleAccessToken
-import com.dope.breaking.model.ResponseLogin
+import com.dope.breaking.model.request.RequestGoogleAccessToken
+import com.dope.breaking.model.request.RequestGoogleToken
+import com.dope.breaking.model.response.ResponseExistLogin
+import com.dope.breaking.model.response.ResponseGoogleAccessToken
+import com.dope.breaking.model.response.ResponseLogin
 import com.dope.breaking.retrofit.RetrofitManager
 import com.dope.breaking.retrofit.RetrofitService
+import com.dope.breaking.util.JwtTokenUtil
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.gson.JsonElement
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
-import okhttp3.Headers
+import org.json.JSONObject
 import retrofit2.Response
 
 class GoogleLogin(private val context: Context) {
     private val jwtHeaderKey = "authorization" // JWT 토큰 검증을 위한 헤더 키 값
-    var responseBody: ResponseLogin? = null // 구글 로그인 프로세스 가운데 얻는 response body 값
+    var responseBody: Any? = null // 구글 로그인 프로세스 가운데 얻는 response body 값
 
     // 구글 로그인 시 여러가지 옵션에 대한 객체
     private val googleSignInOptions: GoogleSignInOptions =
@@ -39,10 +43,11 @@ class GoogleLogin(private val context: Context) {
      * @param account(GoogleSignInAccount): 구글 로그인 시 얻는 계정 정보 객체
      * @return Boolean: 구글 로그인 과정을 통해 사용자가 회원가입이 필요한지 필요하지 않은지에 대한 bool 값 리턴(필요하면 false 리턴)
      * @throws ResponseErrorException: 정상 응답 (2xx) 이외의 응답이 왔을 때 exception 발생
+     * @throws InvalidAccessTokenException: 에러 응답(406), 즉, 플랫폼에 인증할 수 없을 때 발생하는 응답에 대한 예외
      * @author Seunggun Sin
-     * @since 2022-07-07 | 2022-07-11
+     * @since 2022-07-07 | 2022-07-16
      */
-    @Throws(ResponseErrorException::class)
+    @Throws(ResponseErrorException::class, InvalidAccessTokenException::class)
     suspend fun requestGoogleLogin(account: GoogleSignInAccount): Boolean {
         /*
         구글의 엑세스 토큰을 얻기 위해 구글 api 서버에 요청하는 메소드 호출
@@ -58,22 +63,36 @@ class GoogleLogin(private val context: Context) {
         /*
         구글로부터 받아온 id 토큰과 access 토큰으로 백엔드 서버에 로그인을 요청하는 메소드 호출
         */
-        val validationResponse = requestTokenValidation(
-            accessTokenResponseObject.idToken,
-            accessTokenResponseObject.accessToken
-        )
-        if (validationResponse.errorBody() == null) {
-            responseBody = validationResponse.body()!!
-        }
-        if (validationResponse.code() in 200..299) {
-            // 토큰이 있으면 true, 없으면 false 리턴
-            return hasJwtToken(jwtHeaderKey, validationResponse.headers())
-        } else {
-            throw ResponseErrorException(
-                "요청에 실패하였습니다. code: ${validationResponse.code()}\nerror: ${
-                    validationResponse.errorBody()?.string()
-                }"
+        try {
+            val validationResponse = requestTokenValidation(
+                accessTokenResponseObject.idToken,
+                accessTokenResponseObject.accessToken
             )
+            if (validationResponse?.code() in 200..299) {
+                // 토큰이 있으면 true, 없으면 false 리턴
+                val jwtTokenUtil = JwtTokenUtil(context)
+                return if (jwtTokenUtil.hasJwtToken(jwtHeaderKey, validationResponse!!.headers())) {
+                    // 로컬에 저장된 토큰이 없다면 저장하기
+                    if (jwtTokenUtil.getTokenFromLocal() == null) {
+                        jwtTokenUtil.setToken(jwtTokenUtil.getTokenFromResponse(validationResponse.headers())!!)
+                    }
+                    true
+                } else
+                    false
+            } else {
+                // 응답 에러에 대한 예외 발생
+                throw ResponseErrorException(
+                    "요청에 실패하였습니다. code: ${validationResponse!!.code()}\nerror: ${
+                        validationResponse.errorBody()?.string()
+                    }"
+                )
+            }
+        } catch (e: InvalidAccessTokenException) {
+            // 토큰으로 인증할 수 없는 에러에 대한 예외 발생 처리
+            throw InvalidAccessTokenException(e.message!!)
+        } catch (e: ResponseErrorException) {
+            // 예기치 못한 응답 에러에 대한 예외 발생
+            throw ResponseErrorException(e.message!!)
         }
     }
 
@@ -117,14 +136,17 @@ class GoogleLogin(private val context: Context) {
      * ※ 응답의 형태가 여러가지인 경우 Response 방식이 아닌 Call 방식 사용하여 코드에 따른 처리하기
      * @param idToken(String): ID 토큰
      * @param accessToken(String): Access 토큰
-     * @return Response<ResponseLogin> 으로 응답 raw 데이터 반환
+     * @throws ResponseErrorException: 서버에서 지정한 에러 응답이 아닌 경우에 발생하는 예외
+     * @throws InvalidAccessTokenException: 서버에서 토큰을 통해 유저를 인증할 수 없을 때 발생하는 예외
+     * @return Response<JsonElement>? 으로 응답 그 자체를 반환
      * @author Seunggun Sin
-     * @since 2022-07-07 | 2022-07-11
+     * @since 2022-07-07 | 2022-07-16
      */
+    @Throws(ResponseErrorException::class, InvalidAccessTokenException::class)
     private suspend fun requestTokenValidation(
         idToken: String,
         accessToken: String
-    ): Response<ResponseLogin> {
+    ): Response<JsonElement>? {
         return CoroutineScope(Dispatchers.Main).async(Dispatchers.IO) {
             // 백엔드 서버에 대한 retrofit 서비스 객체 생성
             val service = RetrofitManager.retrofit.create(RetrofitService::class.java)
@@ -132,25 +154,32 @@ class GoogleLogin(private val context: Context) {
             parameter 로 받아온 값을 바탕으로 객체를 요청 body 로 전달 후 백엔드 서버에 로그인 요청
             동기적으로 요청 → .execute() 메소드
             */
-            return@async service.requestGoogleLogin(
-                RequestGoogleToken(
-                    idToken,
-                    accessToken
-                )
-            ).execute() // 응답 결과 자체를 리턴
+            val response =
+                service.requestGoogleLogin(
+                    RequestGoogleToken(
+                        idToken,
+                        accessToken
+                    )
+                ).execute()
+            // 먼저 예기치 못한 응답 코드에 대해서 예외 발생 시키기
+            if (response.code() != 406 && response.code() >= 400) {
+                throw ResponseErrorException("요청에 실패하였습니다. code: ${response.code()}\nerror: ${response.errorBody()}")
+            }
+            // 응답으로 온 json string 을 json object 로 변환
+            val responseJson = JSONObject(response.body().toString())
+
+            // 서버에서 지정된 에러에 대한 응답 처리
+            if (responseJson.has("errorMessage")) {
+                throw InvalidAccessTokenException("구글 엑세스 토큰으로 인증할 수 없습니다. error:${responseJson["errorMessage"]}")
+            }
+
+            // 정상 응답에 대한 body 를 구분하여 DTO 저장
+            responseBody = if (responseJson.has("username")) {
+                ResponseLogin.convertJsonToObject(responseJson) // 신규 유저
+            } else {
+                ResponseExistLogin.convertJsonToObject(responseJson) // 기존 유저
+            }
+            return@async response // 응답 결과 자체를 리턴
         }.await() // async 블럭의 코드가 실행될 때까지 기다리고 끝나면 결과 값 리턴
     }
-
-    /**
-     * JWT 토큰이 있는지 없는지 판단. 헤더의 값이 null 이거나 빈 문자열이면 false 리턴, 값이 존재하면 true 리턴.
-     * @param headerName(String): jwt 토큰을 위한 헤더 key 값으로, "authorization"로 고정
-     * @param headers(Headers): 백엔드 서버로 요청의 결과로 받은 응답의 헤더 객체인 response.headers().
-     * @return 응답의 결과로 토큰이 있는지 없는지에 대한 bool 값 리턴
-     * @author Seunggun Sin
-     * @since 2022-07-07
-     */
-    private fun hasJwtToken(headerName: String, headers: Headers): Boolean =
-        // Map 데이터로 인덱스 연산자에 문자열을 넣는 key-value 방식을 사용하여 헤더 map 데이터에 headerName 문자열을 넣었을 때
-        // 나오는 value 의 값이 null 이거나 빈 문자열이라면 false 리턴, 그렇지 않고 정상적인 값이 있으면 true 리턴
-        !(headers[headerName] == null || headers[headerName]!!.isEmpty())
 }
