4185b6852d46dfda0547a0f556b078323b9ba1c5
SeungGun <saverrw2864@naver.com>
2022-07-16 19:31:22 +0900

    chore: conflict resolved by package and comment

---
+++ Breaking/app/src/main/java/com/dope/breaking/model/response/ResponseLogin.kt
@@ -0,0 +1,23 @@
+package com.dope.breaking.model.response
+
+import com.google.gson.annotations.SerializedName
+import org.json.JSONObject
+import java.io.Serializable
+
+data class ResponseLogin(
+    @SerializedName("fullname") var fullName: String, // 유저 성 이름
+    @SerializedName("username") var userName: String, // 회원번호
+    @SerializedName("email") var userEmail: String,   // 유저 이메일
+    @SerializedName("profileImgURL") var profileImgUrl: String // 프로필 이미지 Url
+) : Serializable {
+    companion object {
+        fun convertJsonToObject(jsonObject: JSONObject): ResponseLogin {
+            return ResponseLogin(
+                jsonObject["fullname"] as String,
+                jsonObject["username"] as String,
+                jsonObject["email"] as String,
+                jsonObject["profileImgURL"] as String
+            )
+        }
+    }
+}
