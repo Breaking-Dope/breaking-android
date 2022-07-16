8cf992edb3c22da5b5dd6a24d9957f74760aeaf6
SeungGun <saverrw2864@naver.com>
2022-07-16 19:32:19 +0900

    refactor: conflict resolved and modify type of response on google login

--- Breaking/app/src/main/java/com/dope/breaking/retrofit/RetrofitService.kt
+++ Breaking/app/src/main/java/com/dope/breaking/retrofit/RetrofitService.kt
@@ -1,6 +1,12 @@
 package com.dope.breaking.retrofit
 
-import com.dope.breaking.model.*
+import com.dope.breaking.model.request.RequestGoogleAccessToken
+import com.dope.breaking.model.request.RequestGoogleToken
+import com.dope.breaking.model.request.RequestKakaoToken
+import com.dope.breaking.model.response.ResponseExistLogin
+import com.dope.breaking.model.response.ResponseGoogleAccessToken
+import com.dope.breaking.model.response.ResponseLogin
+import com.google.gson.JsonElement
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
 import retrofit2.Call
@@ -10,13 +16,40 @@ import retrofit2.http.*
 interface RetrofitService {
     /**
     @description - 카카오 로그인 토큰 검증 요청 메소드
-    @param - @Body token : RequestKakaoToken
+    @param - RequestKakaoToken
     @return - Call<KakaoLogin>
     @author - Tae hyun Park
     @since - 2022-07-05 | 2022-07-06
      **/
-    @POST("oauth/sign-in/kakao")
+    @POST("oauth2/sign-in/kakao")
     fun requestKakaoLogin(@Body token: RequestKakaoToken): Call<ResponseLogin>
+
+    /**
+     * 회원가입 시 전화번호 검증 요청 메소드
+     * @request - String
+     * @response - Unit
+     * @author - Tae hyun Park
+     */
+    @GET("/oauth2/sign-up/validate-phone-number/{phoneNumber}")
+    suspend fun requestValidationPhoneNum(@Path("phoneNumber") phoneNumber: String): Response<Unit>
+
+    /**
+     * 회원가입 시 닉네임 검증 요청 메소드
+     * @request - String
+     * @response - Unit
+     * @author - Tae hyun Park
+     */
+    @GET("/oauth2/sign-up/validate-nickname/{nickName}")
+    suspend fun requestValidationNickName(@Path("nickName") nickName: String): Response<Unit>
+
+    /**
+     * 회원가입 시 이메일 검증 요청 메소드
+     * @request - String
+     * @response - Unit
+     * @author - Tae hyun Park
+     */
+    @GET("/oauth2/sign-up/validate-email/{email}")
+    suspend fun requestValidationEmail(@Path("email") email: String): Response<Unit>
 
     /**
      * 구글 로그인 토큰 검증 요청 메소드
@@ -24,8 +57,8 @@ interface RetrofitService {
      * @response ResponseLogin
      * @author Seunggun Sin
      */
-    @POST("oauth/sign-in/google")
-    fun requestGoogleLogin(@Body tokens: RequestGoogleToken): Call<ResponseLogin>
+    @POST("oauth2/sign-in/google")
+    fun requestGoogleLogin(@Body tokens: RequestGoogleToken): Call<JsonElement>
 
     /**
      * 최종 회원가입 요청 메소드 - Multipart 요청
@@ -48,7 +81,7 @@ interface RetrofitService {
      * @author Seunggun Sin
      */
     @POST("oauth2/validate-jwt")
-    suspend fun requestValidationJwt(@Header("Authorization") token: String): Response<ResponseJwtUserInfo>
+    suspend fun requestValidationJwt(@Header("Authorization") token: String): Response<ResponseExistLogin>
 
     /**
      * OAuth2 구글 API 서버로부터 엑세스 토큰 요청 메소드
