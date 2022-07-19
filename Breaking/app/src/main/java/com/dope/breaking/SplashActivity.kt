3cfce8c666903b91e129fe8f1f8e2e266545bf06
SeungGun <saverrw2864@naver.com>
2022-07-19 13:42:07 +0900

    refactor: handling exception for has jwt token but no account

--- Breaking/app/src/main/java/com/dope/breaking/SplashActivity.kt
+++ Breaking/app/src/main/java/com/dope/breaking/SplashActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.dope.breaking.exception.ResponseErrorException
 import com.dope.breaking.util.JwtTokenUtil
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -29,18 +30,25 @@ class SplashActivity : AppCompatActivity() {
 
                 // 로컬에 Jwt 토큰이 저장되어 있다면
                 if (jwtTokenUtil.getTokenFromLocal() != null) {
-                    Log.d(TAG, "TEST1 : "+jwtTokenUtil.getTokenFromLocal())
+                    Log.d(TAG, "TEST1 : " + jwtTokenUtil.getTokenFromLocal())
                     // Jwt 토큰을 이용해 기본 유저 정보 요청
-                    val userData =
-                        jwtTokenUtil.validateJwtToken("Bearer " + jwtTokenUtil.getTokenFromLocal()!!)
+                    try {
+                        val userData =
+                            jwtTokenUtil.validateJwtToken("Bearer " + jwtTokenUtil.getTokenFromLocal()!!)
 
-                    // 메인 페이지로 유저 데이터와 함께 이동
-                    val intent = Intent(applicationContext, MainActivity::class.java)
-                    intent.putExtra("userInfo", userData)
-                    intent.flags =
-                        Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-                    startActivity(intent)
-                    overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                        // 메인 페이지로 유저 데이터와 함께 이동
+                        val intent = Intent(applicationContext, MainActivity::class.java)
+                        intent.putExtra("userInfo", userData)
+                        intent.flags =
+                            Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        startActivity(intent)
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                    } catch (e: ResponseErrorException) { // 토큰은 있는데 계정 정보가 없을 때 처리
+                        val intent = Intent(applicationContext, SignInActivity::class.java)
+                        startActivity(intent)
+                        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
+                        finish()
+                    }
                 } else { // 로컬에 토큰이 저장되어 있지않다면
                     // 로그인 페이지로 이동
                     Log.d(TAG, "TEST2")
