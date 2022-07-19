f855c85a3fe01544a126ac61b5876a4ad32d9914
Taehyuny <hyuny6012@naver.com>
2022-07-19 16:32:13 +0900

    feat: Created drawer's each activity and xml

---
+++ Breaking/app/src/main/java/com/dope/breaking/NaviSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.dope.breaking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NaviSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navi_setting)
+    }
+}
