594353410136ce4a2deedc1dc231bb90d2f834c6
SeungGun <saverrw2864@naver.com>
2022-07-21 17:55:54 +0900

    fix: change type of "userId" property of ResponseExistLogin

--- Breaking/app/src/main/java/com/dope/breaking/model/response/ResponseExistLogin.kt
+++ Breaking/app/src/main/java/com/dope/breaking/model/response/ResponseExistLogin.kt
@@ -5,15 +5,16 @@ import org.json.JSONObject
 import java.io.Serializable
 
 data class ResponseExistLogin(
-    @SerializedName("userId") val userId: Int,
+    @SerializedName("userId") val userId: Long,
     @SerializedName("profileImgURL") val profileImgUrl: String,
     @SerializedName("nickname") val nickname: String,
     @SerializedName("balance") val balance: Int
 ) : Serializable {
     companion object {
+        var baseUserInfo: ResponseExistLogin? = null
         fun convertJsonToObject(jsonObject: JSONObject): ResponseExistLogin {
             return ResponseExistLogin(
-                jsonObject["userId"] as Int,
+                jsonObject["userId"] as Long,
                 jsonObject["profileImgURL"] as String,
                 jsonObject["nickname"] as String,
                 jsonObject["balance"] as Int
