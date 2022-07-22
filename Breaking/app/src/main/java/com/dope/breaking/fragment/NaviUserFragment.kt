de024332e7be557201b810122162a797ba8d8e50
SeungGun <saverrw2864@naver.com>
2022-07-22 16:38:44 +0900

    feat: implement can swipe each post layout using view pager and tab layout in middle section of user page

--- Breaking/app/src/main/java/com/dope/breaking/fragment/NaviUserFragment.kt
+++ Breaking/app/src/main/java/com/dope/breaking/fragment/NaviUserFragment.kt
@@ -3,17 +3,26 @@ package com.dope.breaking.fragment
 import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.dope.breaking.NaviSettingActivity
 import com.dope.breaking.R
+import com.dope.breaking.adapter.UserViewPagerAdapter
 import com.dope.breaking.databinding.FragmentNaviUserBinding
 import com.dope.breaking.model.response.User
 import com.dope.breaking.model.response.ResponseExistLogin
+import com.google.android.material.tabs.TabLayoutMediator
 
 class NaviUserFragment : Fragment() {
     private lateinit var binding: FragmentNaviUserBinding // 바인딩 객체
+    private val tabIconList = arrayOf(
+        R.drawable.ic_baseline_create_24,
+        R.drawable.ic_baseline_add_shopping_cart_24,
+        R.drawable.ic_baseline_bookmark_border_24
+    ) // 게시글 아이콘 리스트
 
     companion object {
         private const val IMAGE_BASE_URL = "https://team-dope.link:8443" // 이미지 호출을 위한 서버 base url
@@ -36,7 +45,7 @@ class NaviUserFragment : Fragment() {
      * 이 Fragment 호출 시 초기 화면에 보여줄 View 세팅 함수
      * @param userData(User): 로딩 Fragment 로 부터 받아온 유저 프로필 객체
      * @author Seunggun Sin
-     * @since 2022-07-21
+     * @since 2022-07-21 | 2022-07-22
      */
     private fun initProfileView(userData: User) {
         /*
@@ -47,6 +56,17 @@ class NaviUserFragment : Fragment() {
         binding.tvFollowValue.text = userData.followingCount.toString()
         binding.tvFollowerValue.text = userData.followerCount.toString()
         binding.tvPostValue.text = userData.postCount.toString()
+
+        val viewPager = binding.viewPager // ViewPager2 객체
+        val tabLayout = binding.tabLayout // TabLayout 객체
+        viewPager.adapter =
+            UserViewPagerAdapter(parentFragmentManager, lifecycle) // ViewPager 어댑터 지정
+
+        // TabLayout 과 ViewPager 를 연결
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            // 각 탭의 아이콘 지정
+            tab.icon = AppCompatResources.getDrawable(requireActivity(), tabIconList[position])
+        }.attach()
 
         binding.myPageToolBar.setOnMenuItemClickListener {
             when (it.itemId) {
