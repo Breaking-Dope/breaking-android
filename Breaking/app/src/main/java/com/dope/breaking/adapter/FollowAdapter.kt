33500a654009b392efa7347e45507fcb18a81120
SeungGun <saverrw2864@naver.com>
2022-08-02 17:37:20 +0900

    feat: implement follow & unfollow in following & follower list (#62)
    * refactor: change field name of user dto class
    
    * refactor: fix profile image setting in user page
    
    * refactor: increase text size and size of button in follow list
    
    * feat: implement follow & unfollow in following & follower list in a various view state by adding isFollowing field

--- Breaking/app/src/main/java/com/dope/breaking/adapter/FollowAdapter.kt
+++ Breaking/app/src/main/java/com/dope/breaking/adapter/FollowAdapter.kt
@@ -11,10 +11,16 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.dope.breaking.R
+import com.dope.breaking.follow.Follow
 import com.dope.breaking.model.FollowData
 import com.dope.breaking.model.response.ResponseExistLogin
 import com.dope.breaking.user.UserProfile
+import com.dope.breaking.util.DialogUtil
+import com.dope.breaking.util.JwtTokenUtil
 import com.dope.breaking.util.ValueUtil
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * 팔로우(워) 리스트 어댑터
@@ -46,30 +52,87 @@ class FollowAdapter(
         holder.imageProfile.setOnClickListener {
             UserProfile(context as Activity).moveToUserPage(data[position].userId)
         }
+        // 리스트 상에서 본인이 들어가 있는 경우 버튼 hidden
         holder.removeButton.visibility =
             if (data[position].userId == ResponseExistLogin.baseUserInfo?.userId) View.GONE else View.VISIBLE
-        //TODO("팔로우, 팔로워 리스트에서 팔로우 & 언팔로우 구현해야함")
-        if (currentUserId == ResponseExistLogin.baseUserInfo?.userId) { // 내 팔로우(워) 리스트인 경우
-            if (state) { // 팔로우 리스트
-                holder.removeButton.text = "팔로잉"
-                holder.removeButton.setOnClickListener {
-                    /*
-                    언팔로우 dialog 띄우고 리스트에서 항목 삭제
-                     */
-//                    removeItem(position)
+
+        val token = ValueUtil.JWT_REQUEST_PREFIX + JwtTokenUtil(context).getTokenFromLocal() // 요청 토큰
+
+        /*
+        로그인 & 비로그인 유저 구분 / 내 팔로우(워) 리스트 & 다른 유저 팔로우(워) 리스트 구분 / 팔로우 & 팔로워 상태 구분
+         */
+        if (ResponseExistLogin.baseUserInfo != null && JwtTokenUtil(context).getTokenFromLocal() != "") { // 로그인한 유저라면
+            if (currentUserId == ResponseExistLogin.baseUserInfo?.userId) { // 내 팔로우(워) 리스트인 경우
+                if (state) { // 팔로우 리스트
+                    convertToFollowingState(holder.removeButton) // 팔로잉 버튼 상태로 전환
+                    // 우측 버튼 클릭 이벤트
+                    holder.removeButton.setOnClickListener {
+                        // 다이얼로그
+                        DialogUtil().MultipleDialog(
+                            context,
+                            "정말 '${data[position].nickname}'님의 팔로우를 취소하시겠습니까?",
+                            "예",
+                            "아니오",
+                            {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    val result =
+                                        Follow().startUnFollowRequest(
+                                            token,
+                                            data[position].userId
+                                        ) // 언팔로우 요청
+                                    if (result) { // 언팔로우 요청 성공 시
+                                        removeItem(position) // 아이템 제거
+                                    } else
+                                        dialogRequestFail()
+                                }
+                            }).show()
+                    }
+                } else { // 팔로워 리스트
+                    holder.removeButton.visibility = View.GONE // 버튼 hidden
                 }
-            } else { // 팔로워 리스트
-                /*
-                상대방의 팔로우를 강제로 끊기
-                 */
-                holder.removeButton.text = "삭제"
+            } else { // 다른 사람 리스트인 경우
+                if (data[position].isFollowing) // 팔로우 중이라면
+                    convertToFollowingState(holder.removeButton) // 팔로잉 버튼 상태로 전환
+                else // 팔로우 중이 아니라면
+                    convertToFollowState(holder.removeButton) // 팔로우 버튼 상태로 전환
+
+                // 우측 버튼 클릭 이벤트
+                holder.removeButton.setOnClickListener {
+                    if (data[position].isFollowing) { // 팔로우 중이라면
+                        DialogUtil().MultipleDialog(
+                            context,
+                            "정말 '${data[position].nickname}'님의 팔로우를 취소하시겠습니까?",
+                            "예",
+                            "아니오",
+                            {
+                                CoroutineScope(Dispatchers.Main).launch {
+                                    val result =
+                                        Follow().startUnFollowRequest(
+                                            token,
+                                            data[position].userId
+                                        ) // 언팔로우 요청
+                                    if (result) { // 언팔로우 요청 성공 시
+                                        convertToFollowState(holder.removeButton) // 버튼 상태 전환
+                                        data[position].isFollowing = false // 팔로잉 상태로 전환
+                                    } else
+                                        dialogRequestFail()
+                                }
+                            }).show()
+                    } else { // 팔로우 중이 아니라면
+                        CoroutineScope(Dispatchers.Main).launch {
+                            val result =
+                                Follow().startFollowRequest(token, data[position].userId) // 팔로우 요청
+                            if (result) { // 팔로우 요청 성공 시
+                                convertToFollowingState(holder.removeButton) // 버튼 상태 전환
+                                data[position].isFollowing = true // 팔로우 상태로 전환
+                            } else
+                                dialogRequestFail()
+                        }
+                    }
+                }
             }
-            holder.removeButton.setBackgroundResource(if (state) R.drawable.feed_category_button_background else R.drawable.sign_up_user_type_selected)
-        } else { // 다른 사람 리스트인 경우
-            /*
-            각각 사람이 나와 팔로우 관계인지 아닌지 판단해서 "팔로우", "팔로잉" 구분
-             */
-            holder.removeButton.text = "팔로잉"
+        } else {
+            holder.removeButton.visibility = View.GONE // 비로그인 유저의 경우 버튼 모두 숨김
         }
     }
 
@@ -79,15 +142,17 @@ class FollowAdapter(
 
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val textNickname: TextView = itemView.findViewById(R.id.tv_following_nickname)
-        val textStatus: TextView = itemView.findViewById(R.id.tv_following_status)
+        private val textStatus: TextView = itemView.findViewById(R.id.tv_following_status)
         val imageProfile: ImageView = itemView.findViewById(R.id.imgv_following_profile)
         val removeButton: Button = itemView.findViewById(R.id.btn_following_remove)
 
         fun bind(item: FollowData) {
             textNickname.text = item.nickname
             textStatus.text = item.statusMsg
-            Glide.with(itemView).load(ValueUtil.IMAGE_BASE_URL + item.profileImgURL)
-                .circleCrop().error(R.drawable.ic_default_profile_image)
+            Glide.with(itemView)
+                .load(ValueUtil.IMAGE_BASE_URL + item.profileImgURL)
+                .circleCrop()
+                .error(R.drawable.ic_default_profile_image)
                 .into(imageProfile)
         }
     }
@@ -103,5 +168,36 @@ class FollowAdapter(
             data.removeAt(position)
             notifyDataSetChanged()
         }
+    }
+
+    /**
+     * "팔로우" 버튼 상태로 전환(텍스트, 배경 변경)
+     * @param button(Button): 우측 버튼 view
+     * @author Seunggun Sin
+     * @since 2022-08-02
+     */
+    private fun convertToFollowState(button: Button) {
+        button.text = "팔로우"
+        button.setBackgroundResource(R.drawable.sign_up_user_type_selected)
+    }
+
+    /**
+     * "팔로잉" 버튼 상태로 전환(텍스트, 배경 변경)
+     * @param button(Button): 우측 버튼 view
+     * @author Seunggun Sin
+     * @since 2022-08-02
+     */
+    private fun convertToFollowingState(button: Button) {
+        button.text = "팔로잉"
+        button.setBackgroundResource(R.drawable.feed_category_button_background)
+    }
+
+    /**
+     * 팔로우, 언팔로우 요청 실패시 띄우는 dialog
+     * @author Seunggun Sin
+     * @since 2022-08-02
+     */
+    private fun dialogRequestFail() {
+        DialogUtil().SingleDialog(context, "요청에 실패하였습니다. 재시도 바랍니다.", "확인").show()
     }
 }
