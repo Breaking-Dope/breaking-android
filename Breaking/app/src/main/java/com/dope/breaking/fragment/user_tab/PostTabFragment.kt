d8ba4433a67264377977336ed8021ad77ab6aa30
SeungGun <saverrw2864@naver.com>
2022-08-10 21:15:51 +0900

    remove: delete temporary test layout for post item

--- Breaking/app/src/main/java/com/dope/breaking/fragment/user_tab/PostTabFragment.kt
+++ Breaking/app/src/main/java/com/dope/breaking/fragment/user_tab/PostTabFragment.kt
@@ -5,10 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.LinearLayout
-import androidx.recyclerview.widget.DividerItemDecoration
-import com.dope.breaking.R
-import com.dope.breaking.adapter.UserPostAdapter
 import com.dope.breaking.databinding.FragmentPostTabBinding
 
 class PostTabFragment : Fragment() {
@@ -19,18 +15,6 @@ class PostTabFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentPostTabBinding.inflate(inflater, container, false)
-        var data = mutableListOf<String>()
-        data.add("/static/compressedProfileImg/d6a7cc89-4874-4298-be1b-48e3e63ed66d.jpg")
-        data.add("/static/compressedProfileImg/d6a2f759-2af3-4645-a3d9-09034eec6c33.jpg")
-
-        val adapter = UserPostAdapter(requireActivity(), data)
-        binding.rcvUserPost.addItemDecoration(
-            DividerItemDecoration(
-                requireActivity(),
-                LinearLayout.VERTICAL
-            )
-        )
-        binding.rcvUserPost.adapter = adapter
         return binding.root
     }
 }
